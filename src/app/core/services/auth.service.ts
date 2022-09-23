055f94d094d9524bd69ea8ae18605a86b9bca5e1
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-09-23 11:56:55 -0300

    <feat> Configuração do Guard do angular e finalização do projeto

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
@@ -34,4 +35,14 @@ export class AuthService {
     localStorage.removeItem('access_token');
     return this.router.navigate(['']);
   }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem("access_token");
+
+    if(!token) return false;
+
+    const jwtHelper = new JwtHelperService();
+    return !jwtHelper.isTokenExpired(token);
+  }
+
 }
