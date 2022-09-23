9744ccd09a67e0414e8ff9ef31404e3a4beaa4ce
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-09-23 10:11:28 -0300

    <feat> Criação dos métodos de validação de autenticação

--- src/app/components/admin/pages/home/home.component.ts
+++ src/app/components/admin/pages/home/home.component.ts
@@ -1,4 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public logout(){
+    this.authService.logout();
   }
 
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
@@ -10,17 +11,27 @@ export class AuthService {
   private url: string = 'http://localhost:3000';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
   public sign(payload: {email: string, password: string}): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
-      map( (data: any) => {
-        return console.log(data)
+    return this.http.post<{token: string}>(`${this.url}/sign`, payload).pipe(
+      map( (res: any) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', JSON.stringify(res.token));
+        return this.router.navigate(['admin']);
       }),
-      catchError((err: any) => {
-        return throwError(err.error.message)
+      catchError((e: any) => {
+        if(e.error.message) return throwError(e.error.message);
+
+        return throwError("Servidor não está respondendo.");
       })
     )
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
   }
 }
