8484b93e5c215564eacc82a6fc05fcb6708d93db
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-09-22 22:02:44 -0300

    <feat> Configuração dos arquivos de estilo

--- src/app/core/components/auth/auth-routing.module.ts
+++ src/app/core/components/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignComponent } from './pages/sign/sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SignComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
